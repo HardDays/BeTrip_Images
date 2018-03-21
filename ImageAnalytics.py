16753c20ba2341df0dc480b2ac36ad5f4b6fffe6
Maria <marikoreneva@gmail.com>
2018-03-21 20:08:24 +0300

    Added comments

--- ImageAnalytics.py
+++ ImageAnalytics.py
@@ -10,23 +10,34 @@ class ImageAnalytics:
         self.bc = BeautyClassificator()
     
     def get_score(self, img: Image):
-        score = 0
-        score += self._face_score(img)
-        img = prepare_bn_classif(img)
-        score += self._beauty_score(img)
-        return score
+        """
+        Returns score for image depending on it's beauty and containing of faces
+        :param img: Image (PIL)
+        :return: score in range [0,2]
+        """
+        return self._face_score(img) + self._beauty_score(prepare_bn_classif(img))
 
     def _face_score(self, img):
+        """
+        Gives image score 0 if it contains faces and 1 otherwise
+        :param img: Image
+        :return: 0 or 1
+        """
         face = self.fd.is_with_faces(img)
         return int(not face)
 
     def _beauty_score(self, img):
+        """
+        Gives image a score according to its beauty. Higher score corresponds to more beautiful images.
+        :param img: Image
+        :return: score in range [0,1]
+        """
         beautiful, prob = self.bc.is_beautiful(img)
         return int(beautiful) * prob
 
 
-from image_analysis.image_helper import open_img
-ia = ImageAnalytics()
-im = open_img("/Users/maria/BeTrip/instagram/1/1/1.jpg")
-score = ia.get_score(im)
-print(score)
+# from image_analysis.image_helper import open_img
+# ia = ImageAnalytics()
+# im = open_img("/Users/maria/BeTrip/instagram/1/1/1.jpg")
+# score = ia.get_score(im)
+# print(score)
